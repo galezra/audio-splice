5438376fcc3976119a11c0ef1d31d26461ddf612
naluconcepcion <naluconcepcion@gmail.com>
2018-02-07 12:55:12 -0500

    Added component which measures total time, and bytes between possible splits.

--- Testers/Cut.java
+++ Testers/Cut.java
@@ -1,57 +1,72 @@
 import java.util.ArrayList;
-public class Cut {;
-  private double[] preSplit;
-  private double[] splits;
+public class Cut {
+// Fields
+  public static final double SAMPLE_RATE = 22050.0;
+  private double[] preSplit; // array of doubles generated prior to being stripped
+  private double[] splits; // array of doubles which are potential splittable times
+  private double[] times; // !!!!! TIMES FOR POSSIBLE SPLIT!! HOW TO DETERMINE??
+  private double totTime; // total time of reading in file
+  private ArrayList<Integer> numPrev = new ArrayList<Integer>(); // holds # doubles between splits, useful for conversion to time data
+// Instantiation
   public Cut(String fn) {
     preSplit = StdAudio.read(fn);
-    splits = possiSplit(fn);
+    splits = possiSplits(fn);
+    totTime = (StdAudio.readByte(fn)).length/SAMPLE_RATE;
+    // times = convertTime(splits);
   }
-  private double[] possiSplit(String filename) {
+// Heavy Algorithms Section
+  private double[] possiSplits(String filename) {
     ArrayList<Double> lessZeroes = new ArrayList();
     ArrayList<Double> greaterZeroes = new ArrayList();
     ArrayList<Double> zeroes = new ArrayList();
-    double[] arrZeroes;
+
+    // section for setting numPrev
+    int prevs = 0;
     for(int i = 0; i < preSplit.length; i++) {
       double val = preSplit[i];
+      prevs++;
       if(val < 0) {
         lessZeroes.add(val);
+        // numPrev.add(prevs);
+        // prevs = 0;
       }
       else if(val > 0) {
         greaterZeroes.add(val);
+        // numPrev.add(prevs);
+        // prevs = 0;
       }
       else {
         zeroes.add(val);
+        numPrev.add(prevs);
+        prevs = 0;
       }
     }
+    double[] arrZeroes;
     arrZeroes = new double[greaterZeroes.size()];
     for(int i = 0; i < greaterZeroes.size(); i++) {
       arrZeroes[i] = greaterZeroes.get(i);
     }
     return arrZeroes;
   }
+  // private double[] likelySplit(double[] arr) {
+  //   for(int i = 0; i < arr.length; i++) {
+  //     if(arr[i] )
+  //   }
+  // }
+// Getters
+  private double[] convertTime(double[] arr) { // I really need to figure out how to convert from a particular byte to a time
+    
+  }
   public double[] getSplits() {
     return splits;
   }
-  // public double[] pairTimes(double[] splits) {
-  //
-  // }
+  public double getFileTime() {
+    return totTime;
+  }
   public static void main(String[] args) {
     String tempFile = "1-welcome.wav";
     Cut c = new Cut(tempFile);
     double[] tempSplits = c.getSplits();
-
-    int SAMPLE_RATE = 44100;
-    double freq = 440.0;
-    for (int i = 0; i <= SAMPLE_RATE; i++) {
-        StdAudio.play(0.5 * Math.sin(2 * Math.PI * tempSplits[i] / SAMPLE_RATE));
-    }
-
-    // System.out.print("[");
-    // for(int i = 0; i < tempSplits.length; i++) {
-    //   System.out.print(tempSplits[i] + ", ");
-    // }
-    // System.out.print("]");
-    System.out.println(tempSplits.length);
-    StdAudio.close();
+    System.out.println(c.numPrev);
   }
 }
